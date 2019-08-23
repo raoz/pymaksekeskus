847364c18d807f83667a8cd9dd710bf04e6f88bb
Rao Zvorovski <rao.zvorovski@outlook.com>
2019-08-23 19:03:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maksekeskus",
+    version="0.0.1",
+    author="Rao Zvorovski",
+    author_email="rao.zvorovski@codeduf.eu",
+    description="Library to use the Maksekeskus(Makecommerce) APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raoz/pymaksekeskus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Office/Business :: Financial"
+    ],
+)
